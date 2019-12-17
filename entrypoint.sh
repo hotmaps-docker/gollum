c2f190dc8c4a9b5f07f9db52a0b6c269f4c1ecf0
hud <authentication.dh@gmail.com>
2019-12-17 10:46:29 +0100

    fix git config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
+# git config
+git config --global user.email $GIT_USER_EMAIL
+git config --global user.name $GIT_USER_NAME
+
+# run
 service cron start
 /usr/local/bin/gollum /root/wikidata --config config.rb --port 80
