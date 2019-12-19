add30ae6b1f03764de0233ea9c654232d63b775f
hud <authentication.dh@gmail.com>
2019-12-19 14:42:08 +0100

    allow gollum to use other branches
    configuration in .env file (see .env.example)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ git config --global user.name $GIT_USER_NAME
 
 # run
 service cron start
-/usr/local/bin/gollum /root/wikidata --config config.rb --port 80
+/usr/local/bin/gollum /root/wikidata --config config.rb --port 80 --ref $GIT_BRANCH
